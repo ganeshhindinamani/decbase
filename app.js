9cd3ef591959a88ea86d5c8cec81a715ea8b3656
Ganesh Ramesh Hindinamani <ganhindinamani@gmail.com>
2021-05-26 00:58:44 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+// const links = document.querySelectorAll(".nav-links a");
+
+// links.forEach((link) => {
+//   link.addEventListener("click", () => {
+//     links.forEach((link2) => {
+//       if (link2.classList[0] !== "active-link") {
+//         console.log("hello");
+//         link2.classList.add("active-link");
+//       } else {
+//         link2.classList.remove("active-link");
+//       }
+//     });
+//   });
+// });
+
+// console.log(links[0].classList);
+
+// const currentLocation = location.href;
+// const links = document.querySelectorAll(".nav-links a");
+
+// const linksLength = links.length;
+// for (let i = 0; i < linksLength; i++) {
+//   console.log(currentLocation);
+//   if (links[i].href === currentLocation) {
+//     links[i].className = "active";
+//   }
+// }
